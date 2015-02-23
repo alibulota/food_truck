a5833a5d6bcffa9f5ded71a8dc9434ee616d03d8
James Warren <jwarren116@gmail.com>
2015-02-23 09:53:52 -0800

    Initial scaffold creation of Pyramid with SQLAlchemy

---
+++ FoodTruck/setup.py
@@ -0,0 +1,47 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='FoodTruck',
+      version='0.0',
+      description='FoodTruck',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='foodtruck',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = foodtruck:main
+      [console_scripts]
+      initialize_FoodTruck_db = foodtruck.scripts.initializedb:main
+      """,
+      )
