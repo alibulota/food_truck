c09d80ffb68513b0dd0de4c3660b80e00d765ab4
James Warren <jwarren116@gmail.com>
2015-02-24 12:20:57 -0800

    Change column name from food_type to cuisine to reduce confusion; templates updated

--- foodtruck/models.py
+++ foodtruck/models.py
@@ -7,7 +7,7 @@ from sqlalchemy import (
     Time,
     ForeignKey,
     )
-
+from webhelpers.text import urlify
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import (
@@ -25,12 +25,16 @@ Base = declarative_base()
 class Truck(Base):
     __tablename__ = 'trucks'
     id = Column(Integer, primary_key=True)
-    name = Column(Text, nullable=False)
+    name = Column(Text, nullable=False, unique=True)
     location = relationship('Locations')
-    food_type = Column(Text)
+    cuisine = Column(Text)
     payment = Column(Text)
     twitter = Column(Text)
     website = Column(Text)
+
+    @property
+    def slug(self):
+        return urlify(self.name)
 
     @classmethod
     def all(cls):
@@ -39,11 +43,11 @@ class Truck(Base):
     @classmethod
     def add_truck(cls, request):
         name = request.params.get('name', None)
-        food_type = request.params.get('cuisine', None)
+        cuisine = request.params.get('cuisine', None)
         payment = request.params.get('payment', None)
         twitter = request.params.get('twitter', None)
         website = request.params.get('website', None)
-        new_entry = cls(name=name, food_type=food_type, payment=payment,
+        new_entry = cls(name=name, cuisine=cuisine, payment=payment,
                         twitter=twitter, website=website)
         DBSession.add(new_entry)
 
